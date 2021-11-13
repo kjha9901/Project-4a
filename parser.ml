37903876e5fead7a14b3ad228ead17b3a2991ca0
kjha9901 <90051819+kjha9901@users.noreply.github.com>
2021-11-12 19:38:13 -0500

    Add files via upload

--- parser.ml
+++ parser.ml
@@ -34,28 +34,121 @@ let rec lookahead_many (toks: token list) (n: int) =
 
 
 (* Part 2: Parsing expressions *)
-(*
-let rec parse_Expressions toks =
-  
-  let a1 = parse_Or toks in
-  let t = lookahead toks in
-  match t with
-  | Tok_If -> 
-    match_token t Tok_If
-    let a2 = parse_Expressions t in
-      
-;;
-*)
 
 let rec parse_Prime toks = 
   match lookahead toks with
-  | Some Tok_Int i -> let tok2 = match_token toks (Tok_Int i) in (tok2, Value(Int i))
-  | Some Tok_Bool b -> let tok2 = match_token toks (Tok_Bool b) in (tok2, Value(Bool b))
-  | Some Tok_String s -> let tok2 = match_token toks (Tok_String s) in (tok2, Value(String s))
-  | Some Tok_ID d -> let tok2 = match_token toks (Tok_ID d) in (tok2, ID d)
-  | Some Tok_LParen -> let tok2 = match_token toks (Tok_LParen) in
+  
+  | Some Tok_Int i ->
+    let tok2 = match_token toks (Tok_Int i) in
+      (match lookahead tok2 with
+      | Some Tok_Int i2 ->
+        let tok3 = match_token tok2 (Tok_Int i2) in
+        (tok3, FunctionCall(Value(Int i),Value(Int i2)))
+      | Some Tok_Bool b2 ->
+        let tok3 = match_token tok2 (Tok_Bool b2) in
+        (tok3, FunctionCall(Value(Int i),Value(Bool b2)))
+      | Some Tok_String s2 ->
+        let tok3 = match_token tok2 (Tok_String s2) in
+        (tok3, FunctionCall(Value(Int i),Value(String s2)))
+      | Some Tok_ID d2 ->
+        let tok3 = match_token tok2 (Tok_ID d2) in
+        (tok3, FunctionCall(Value(Int i),ID d2))
+      | Some Tok_LParen ->
+        let tok3 = match_token tok2 (Tok_LParen) in
+        let (tok4, expr) = parse_Or tok3 in
+        let tok5 = match_token tok4 Tok_RParen in
+        (tok5, FunctionCall(Value(Int i),expr))
+      | _ -> (tok2, Value(Int i)))
+  
+  | Some Tok_Bool b ->
+    let tok2 = match_token toks (Tok_Bool b) in
+    (match lookahead tok2 with
+      | Some Tok_Int i2 ->
+        let tok3 = match_token tok2 (Tok_Int i2) in
+        (tok3, FunctionCall(Value(Bool b),Value(Int i2)))
+      | Some Tok_Bool b2 ->
+        let tok3 = match_token tok2 (Tok_Bool b2) in
+        (tok3, FunctionCall(Value(Bool b),Value(Bool b2)))
+      | Some Tok_String s2 ->
+        let tok3 = match_token tok2 (Tok_String s2) in
+        (tok3, FunctionCall(Value(Bool b),Value(String s2)))
+      | Some Tok_ID d2 ->
+        let tok3 = match_token tok2 (Tok_ID d2) in
+        (tok3, FunctionCall(Value(Bool b),ID d2))
+      | Some Tok_LParen ->
+        let tok3 = match_token tok2 (Tok_LParen) in
+        let (tok4, expr) = parse_Or tok3 in
+        let tok5 = match_token tok4 Tok_RParen in
+        (tok5, FunctionCall(Value(Bool b),expr))
+      | _ -> (tok2, Value(Bool b)))
+  
+  | Some Tok_String s ->
+    let tok2 = match_token toks (Tok_String s) in
+    (match lookahead tok2 with
+      | Some Tok_Int i2 ->
+        let tok3 = match_token tok2 (Tok_Int i2) in
+        (tok3, FunctionCall(Value(String s),Value(Int i2)))
+      | Some Tok_Bool b2 ->
+        let tok3 = match_token tok2 (Tok_Bool b2) in
+        (tok3, FunctionCall(Value(String s),Value(Bool b2)))
+      | Some Tok_String s2 ->
+        let tok3 = match_token tok2 (Tok_String s2) in
+        (tok3, FunctionCall(Value(String s),Value(String s2)))
+      | Some Tok_ID d2 ->
+        let tok3 = match_token tok2 (Tok_ID d2) in
+        (tok3, FunctionCall(Value(String s),ID d2))
+      | Some Tok_LParen ->
+        let tok3 = match_token tok2 (Tok_LParen) in
+        let (tok4, expr) = parse_Or tok3 in
+        let tok5 = match_token tok4 Tok_RParen in
+        (tok5, FunctionCall(Value(String s),expr))
+      | _ -> (tok2, Value(String s)))
+  
+  | Some Tok_ID d ->
+    let tok2 = match_token toks (Tok_ID d) in
+      (match lookahead tok2 with
+      | Some Tok_Int i2 ->
+        let tok3 = match_token tok2 (Tok_Int i2) in
+        (tok3, FunctionCall(ID d,Value(Int i2)))
+      | Some Tok_Bool b2 ->
+        let tok3 = match_token tok2 (Tok_Bool b2) in
+        (tok3, FunctionCall(ID d,Value(Bool b2)))
+      | Some Tok_String s2 ->
+        let tok3 = match_token tok2 (Tok_String s2) in
+        (tok3, FunctionCall(ID d,Value(String s2)))
+      | Some Tok_ID d2 ->
+        let tok3 = match_token tok2 (Tok_ID d2) in
+        (tok3, FunctionCall(ID d,ID d2))
+      | Some Tok_LParen ->
+        let tok3 = match_token tok2 (Tok_LParen) in
+        let (tok4, expr) = parse_Or tok3 in
+        let tok5 = match_token tok4 Tok_RParen in
+        (tok5, FunctionCall(ID d,expr))
+      | _ -> (tok2, ID d))
+  
+  | Some Tok_LParen ->
+    let tok2 = match_token toks (Tok_LParen) in
     let (tok3, expr) = parse_Or tok2 in
-    let tok4 = match_token tok3 (Tok_RParen) in (tok4, expr)
+    let tok4 = match_token tok3 (Tok_RParen) in
+    (match lookahead tok4 with
+      | Some Tok_Int i2 ->
+        let tok5 = match_token tok4 (Tok_Int i2) in
+        (tok5, FunctionCall(expr,Value(Int i2)))
+      | Some Tok_Bool b2 ->
+        let tok5 = match_token tok4 (Tok_Bool b2) in
+        (tok5, FunctionCall(expr,Value(Bool b2)))
+      | Some Tok_String s2 ->
+        let tok5 = match_token tok4 (Tok_String s2) in
+        (tok5, FunctionCall(expr,Value(String s2)))
+      | Some Tok_ID d2 ->
+        let tok5 = match_token tok4 (Tok_ID d2) in
+        (tok5, FunctionCall(expr,ID d2))
+      | Some Tok_LParen ->
+        let tok5 = match_token tok4 (Tok_LParen) in
+        let (tok6, expr2) = parse_Or tok5 in
+        let tok7 = match_token tok4 Tok_RParen in
+        (tok7, FunctionCall(expr,expr2))
+      | _ -> (tok4, expr))
     
   | Some Tok_If ->
     let tok2 = match_token toks Tok_If in
@@ -75,24 +168,31 @@ let rec parse_Prime toks =
       let (tok5, exprTail) = parse_Or tok4 in
       (tok5, Fun(id, exprTail))
     | _ -> raise (InvalidInputException("Invalid Tok_Fun input")))
-  (*
-  | Some Tok_Let
   
-  *)
+  | Some Tok_Let ->
+    let tok2 = match_token toks Tok_Let in
+    (match (lookahead tok2) with
+    | Some Tok_Rec ->
+      let tok3 = match_token tok2 Tok_Rec in
+      (match (lookahead tok3) with
+      | Some Tok_ID(id) ->
+        let tok4 = match_token tok3 (Tok_ID(id)) in
+        let tok5 = match_token tok4 Tok_Equal in
+        let (tok6,expr) = parse_Or tok5 in
+        let tok7 = match_token tok6 Tok_In in
+        let (tok8, expr2) = parse_Or tok7 in
+        (tok8, Let(id,true,expr,expr2))
+      | _ -> raise (InvalidInputException("Invalid Let input")))
+    | Some Tok_ID(id) ->
+      let tok3 = match_token tok2 (Tok_ID(id)) in
+      let tok4 = match_token tok3 Tok_Equal in
+      let (tok5,expr) = parse_Or tok4 in
+      let tok6 = match_token tok5 Tok_In in
+      let (tok7, expr2) = parse_Or tok6 in
+      (tok7, Let(id,false,expr,expr2))
+    | _ -> raise (InvalidInputException("Invalid Let input")))
+      
   | _ -> raise (InvalidInputException("Invalid parse input"))
-
-(*
-and parse_FunctionCall toks =
-  let (toksTail, expr) = parse_Prime toks in
-  match lookahead toksTail with
-  | Some Tok_Int i -> if 
-    
-  | Some Tok_Bool b ->
-  | Some Tok_String s ->
-  | Some Tok_ID d ->
-  | Some Tok_LParen -> 
-  | _ -> parse_Prime toks
-  *)
 
 
 and parse_Not toks =
@@ -189,67 +289,6 @@ and parse_Or toks =
     (tok3, Binop(Or, expr, exprTail))
   | _ -> (toksTail, expr)
 
-(*
-and parse_If toks =
-  let (toksTail, expr) = parse_Or toks in
-  match (lookahead toksTail) with
-  | Some Tok_If ->
-    let tok2 = match_token toksTail Tok_If in
-    let (toksTailExpr1, expr1) = parse_If tok2 in
-  | Some Tok_Then ->
-    let tok3 = match_token toks Tok_Then in
-    let (toksTailExpr2, expr2) = parse_If tok3 in
-  | Some Tok_Else ->    
-    let tok4 = match_token toks Tok_Else in
-    let (toksTailExpr3, expr3) = parse_If tok4 in
-    
-    (*let (toksTail, expr) = parse_If toksTailExpr3 in*)
-    (toksTailExpr3, If(expr1, expr2, expr3))
-    
-  | _ -> (toksTail, expr)
-
-and parse_Fun toks =
-  let (toksTail, expr) = parse_If toks in
-  match (lookahead toksTail) with
-  | Some Tok_Fun ->
-    let tok2 = match_token toksTail Tok_Fun in
-    (match (lookahead tok2) with
-    | Some Tok_ID(id) ->
-      let tok3 = match_token tok2 (Tok_ID(id)) in
-      let tok4 = match_token tok3 Tok_Arrow in
-      let (tok5, exprTail) = parse_Fun tok4 in
-      (tok5, Fun(id, exprTail))
-    | _ -> raise (InvalidInputException("Invalid Func input")))
-  | _ -> (toksTail, expr)
-
-and parse_Let toks =
-  let (toksTail, expr) = parse_Fun toks in
-  match (lookahead toksTail) with
-  | Some Tok_Let ->
-    let tok2 = match_token toksTail Tok_Let in
-    if (lookahead tok2) = Some Tok_Rec then
-      let tok3 = match_token tok2 Tok_Rec in
-      (match (lookahead tok3) with
-      | Some Tok_ID(id) ->
-        let tok4 = match_token tok3 (Tok_ID(id)) in
-        let tok5 = match_token tok4 Tok_Equal in
-        let (toksTailExpr, expr) = parse_Let tok5 in
-        let tok6 = match_token toksTailExpr Tok_In in
-        let (toksTailExpr2, expr2) = parse_Let tok6 in
-        (toksTailExpr2, Let(id, true, expr, expr2))
-      | _ -> raise (InvalidInputException("Invalid Let input")))
-    else
-      (match (lookahead tok2) with
-      | Some Tok_ID(id) ->
-        let tok3 = match_token tok2 (Tok_ID(id)) in
-        let tok4 = match_token tok3 Tok_Equal in
-        let (toksTailExpr, expr) = parse_Let tok4 in
-        let tok5 = match_token toksTailExpr Tok_In in
-        let (toksTailExpr2, expr2) = parse_Let tok5 in
-        (toksTailExpr2, Let(id, false, expr, expr2))
-      | _ -> raise (InvalidInputException("Invalid Let input")))
-  | _ -> (toksTail, expr)
-*)
 ;;
 
 let rec parse_expr toks = 
@@ -258,7 +297,30 @@ let rec parse_expr toks =
 
 (* Part 3: Parsing mutop *)
 
-let rec parse_mutop toks = failwith "unimplemented"
+let rec parse_mutopDoubleSemi toks =
+  match lookahead toks with
+  
+  | Some Tok_DoubleSemi ->
+    let tok2 = match_token toks Tok_DoubleSemi in
+    (toks2, NoOp)
+  
+  | Some Tok_Def ->
+    let tok2 = match_token toks Tok_Def in
+    (match (lookahead tok2) with
+    | Some Tok_ID(id) ->
+      let tok3 = match_token tok2 (Tok_ID(id)) in
+    | _ -> raise (InvalidInputException("Invalid Tok_Def input")))
+    
+  | _ -> parse_expr toks
+
+and parse_Mutop toks =
+
+
+;;
+
+let rec parse_mutop toks = 
+  parse_mutopDoubleSemi toks
+;;
 
 
 
